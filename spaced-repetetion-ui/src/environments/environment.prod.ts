b9ed91fba7cdfbac899d3c72a49c1bb09aaad3bf
Hasan Burak Songur <123594944+hburaks@users.noreply.github.com>
2025-02-23 13:20:42 +0300

    Delete docker files for db connection

--- spaced-repetetion-ui/src/environments/environment.prod.ts
+++ spaced-repetetion-ui/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.your-domain.com/api',
+  apiUrl: 'http://localhost:8080/api', // Update this when you decide on deployment
 };
