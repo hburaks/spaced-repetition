7f9cc9c60052e244cc2db9c0bb7f391deb8aa345
Hasan Burak Songur <123594944+hburaks@users.noreply.github.com>
2025-03-01 15:55:04 +0300

    fix import

--- spaced-repetetion-be/src/main/java/com/hburak_dev/spaced_repetition_be/card/dto/CreateCardRequest.java
+++ spaced-repetetion-be/src/main/java/com/hburak_dev/spaced_repetition_be/card/dto/CreateCardRequest.java
@@ -1,9 +1,8 @@
 package com.hburak_dev.spaced_repetition_be.card.dto;
 
-import lombok.Data;
-
 import java.util.List;
-import java.util.Set;
+
+import lombok.Data;
 
 @Data
 public class CreateCardRequest {
